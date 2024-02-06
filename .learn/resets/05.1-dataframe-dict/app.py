e7d77728d6e743266283e5aad1cf89fe2aa1a884
Susana Florêncio <101977568+susanaf2000@users.noreply.github.com>
2024-02-06 21:29:59 +0000

    Exercises done

---
+++ .learn/resets/05.1-dataframe-dict/app.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+
+data = [
+    { 
+        "brand": "Toyota", 
+        "make": "Corolla",
+        "color": "Blue"
+    },
+    {
+        "brand": "Ford", 
+        "make": "K",
+        "color": "Yellow"
+    },
+    {
+        "brand": "Porche", 
+        "make": "Cayenne",
+        "color": "White"
+    }
+    
+]
